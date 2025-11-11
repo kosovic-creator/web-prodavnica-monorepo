40b77432380fcaf414f76181c081635b71d15b4e
kosovic-creator <drasko.kosovic@icloud.com>
2025-11-12 00:05:02 +0100

    bb

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -1,15 +1,13 @@
 import "./globals.css";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 export default function RootLayout({
   children,
-  params,
 }: {
   children: ReactNode;
-  params: { locale: string };
 }) {
   return (
-    <html lang={params.locale}>
+    <html >
       <body>{children}</body>
     </html>
   );
